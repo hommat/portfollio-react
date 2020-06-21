1c0b3c5fb7595193363f5e9291cbcf967b70ea40
hommat <zmateusz1111@gmail.com>
2020-06-21 13:45:15 +0200

    Add redux skill

--- src/components/skills/SkillsetFine.tsx
+++ src/components/skills/SkillsetFine.tsx
@@ -4,6 +4,7 @@ import Skillset from "./Skillset";
 import Skill from "./Skill";
 
 import { ReactComponent as TypescriptSVG } from "../../assets/svg/ts.svg";
+import { ReactComponent as ReduxSVG } from "../../assets/svg/redux.svg";
 import { ReactComponent as SassSVG } from "../../assets/svg/sass.svg";
 import { ReactComponent as MaterialUiSVG } from "../../assets/svg/mui.svg";
 
@@ -11,6 +12,7 @@ const SkillsetFine = () => {
   return (
     <Skillset heading="I feel fine in">
       <Skill name="Typescript" Svg={TypescriptSVG} />
+      <Skill name="Redux" Svg={ReduxSVG} />
       <Skill name="Sass" Svg={SassSVG} />
       <Skill name="Material UI" Svg={MaterialUiSVG} />
     </Skillset>
